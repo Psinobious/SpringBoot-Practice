236ae8ac9d2b0b896cd4b3928a46cc2407d1ed1d
Psinobious <daniel22.daj@gmail.com>
2018-11-05 14:36:34 -0800

    Add files via upload

---
+++ SpringBootPractice2/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@EnableAutoConfiguration
+@Controller
+public class DemoApplication {
+    @RequestMapping("/")
+    @ResponseBody
+    String hello(){
+            return "Hello World";
+    }
+    String test(){
+        return "This is  a test";
+    }
+    public static void main(String[] args) {
+	SpringApplication.run(DemoApplication.class, args);
+                
+    }
+}
